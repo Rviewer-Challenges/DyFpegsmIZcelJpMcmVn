84d6c4c8037349fe1c2f01bc1a65236349e53e18
Sergio Guerra <sergioguerra1991@gmail.com>
2022-06-27 14:54:42 -0500

    Build: Left pane responsiveness

--- src/components/TweetNavigation.svelte
+++ src/components/TweetNavigation.svelte
@@ -2,6 +2,8 @@
     import TweetButton from './TweetButton.svelte'
     import TweetIcon from './TweetIcon.svelte'
 
+    export let className = ''
+
     const NAVIGATION_ITEM_TYPES = {
       ImageOnly: 'ImageOnly',
       Image: 'Image',
@@ -11,7 +13,7 @@
 
     const navigationList = [
       {
-        label: 'Twitter', type: NAVIGATION_ITEM_TYPES.ImageOnly, icon: 'https://cdn-icons-png.flaticon.com/512/733/733579.png',
+        label: 'Twitter', type: NAVIGATION_ITEM_TYPES.ImageOnly, icon: 'https://cdn-icons-png.flaticon.com/512/733/733635.png',
       },
       {
         label: 'Inicio', type: NAVIGATION_ITEM_TYPES.FontIcon, icon: 'e88a', selected: true,
@@ -23,20 +25,23 @@
       { label: 'Listas', type: NAVIGATION_ITEM_TYPES.FontIcon, icon: 'e0ee' },
       { label: 'Perfil', type: NAVIGATION_ITEM_TYPES.FontIcon, icon: 'f0d3' },
       { label: 'Más Opciones', type: NAVIGATION_ITEM_TYPES.FontIcon, icon: 'e619' },
-      { label: 'Twittear', type: NAVIGATION_ITEM_TYPES.Button },
+      { label: 'Twittear', type: NAVIGATION_ITEM_TYPES.Button, icon: 'https://cdn-icons-png.flaticon.com/512/44/44870.png' },
     ]
 </script>
 
-<section class="relative w-2/5 float-right">
-    <ul class="cursor-default fixed">
+<section class="{className}">
+    <ul class="cursor-default">
         {#each navigationList as navigationItem}
             <li class="py-1 {navigationItem.selected ? 'font-bold' : ''}">
                 {#if navigationItem.type === NAVIGATION_ITEM_TYPES.ImageOnly}
-                    <button class="hover:bg-sky-900/25 p-3 rounded-full">
-                        <img src={navigationItem.icon} alt={navigationItem.label} class="w-7 grayscale brightness-200 contrast-200">
+                    <button class="hover:bg-sky-900/25  w-12 aspect-square p-3 ml-3 rounded-full">
+                        <img src={navigationItem.icon} alt={navigationItem.label} class="w-7 grayscale invert">
                     </button>
                 {:else if navigationItem.type === NAVIGATION_ITEM_TYPES.Button}
-                    <TweetButton className="w-full py-3 " value={navigationItem.label}/>
+                    <TweetButton className="w-56 h-12 hidden lg:inline" value={navigationItem.label}/>
+                    <button class="hover:bg-sky-500/90 bg-sky-500 w-12 aspect-square p-3 ml-3 rounded-full self-center inline lg:hidden">
+                        <img src={navigationItem.icon} alt={navigationItem.label} class="w-7 grayscale invert">
+                    </button>
                 {:else}
                     <button class="hover:bg-neutral-900 rounded-3xl inline py-1.5 px-3">
                         {#if navigationItem.type === NAVIGATION_ITEM_TYPES.Image || navigationItem.type === NAVIGATION_ITEM_TYPES.ImageOnly}
@@ -44,7 +49,7 @@
                         {:else if navigationItem.type === NAVIGATION_ITEM_TYPES.FontIcon}
                             <TweetIcon icon={navigationItem.icon} className="align-middle pb-1 pl-1 text-3xl" variation={navigationItem.selected ? 'rounded' : 'outlined'}/>
                         {/if}
-                        <span class="mx-4 text-xl">{navigationItem.label}</span>
+                        <span class="mx-4 text-xl hidden lg:inline">{navigationItem.label}</span>
                     </button>
                 {/if}
             </li>
--- src/views/LeftPane.svelte
+++ src/views/LeftPane.svelte
@@ -2,6 +2,6 @@
     import TweetNavigation from '../components/TweetNavigation.svelte'
 </script>
 
-<section class="border-r border-zinc-700 pr-4">
-    <TweetNavigation/>
+<section class="border-r border-zinc-700 pr-4 relative w-full">
+    <TweetNavigation className="sticky right-0 top-0 float-right w-14 lg:w-56"/>
 </section>
